c7a02bb91fdc8ed72670ba7a5c6d61fd45a0c8ad
Meekah Yzabelle A. Carballo <91593459+macarballo@users.noreply.github.com>
2023-04-19 01:05:30 +0800

    Updated Carballo_Pelayo_PE04.rkt
    Added a few comments to the code and modified the function definitions for B and C. That said, I also removed the 'display' function where the functions to be called along their corresponding input lists are placed inside (para mas distinguishable sa terminal yung answer which is in color blue na font). Pa-double check lang ako ng mga modifications and the whole code, in general, kasi baka may na-miss ako na information about the specification. Thank you! <3

--- Carballo_Pelayo_PE04.rkt
+++ Carballo_Pelayo_PE04.rkt
@@ -1,5 +1,9 @@
 #lang scheme
-;A: Sum of Numbers :Care of Satori
+; Programming Exercise 04: Scheme Programming 2
+; Developed by: Meekah Yzabelle A. Carballo (2021-05127)
+;               Khublei Mo Satori Pelayo (2021-00529)
+
+; A: Sum of Numbers: Care of Satori
 (define (nested-sums lst)
   ;checks if the given lst is empty or not
   (cond ((null? lst) 0)
@@ -14,46 +18,43 @@
   ;gives out the sum
   (nested-sums lis))
 
-;calling the my-sums function which, in turn, calls on the nested-sums function
-;that will then add the elements of the given array
+; calling the my-sums function which, in turn, calls on the nested-sums function
+; that will then add the elements of the given array
 (display "PART A: Sum of Numbers")
 (newline)
-(display "The Sum of the numbers (1 2 3 (4 5) 6 (7 (8) 9) is (")
+(display "The Sum of the numbers (1 2 3 (4 5) 6 (7 (8) 9)) is ")
                  ;v is included for the following input to be taken literally
-(display (my-sums '(1 2 3 (4 5) 6 (7 (8) 9))))
-(display ")")
-(newline)(newline)
+(my-sums '(1 2 3 (4 5) 6 (7 (8) 9)))
+(newline)
 
 ; B. Reverse List 1: Care of Meekah
-(define (my-reverse1 lis) ; Takes an input list and returns a new list with the same elements in order.
-  (define (reverse lis)
-    (define (reverse-helper lis acc) ; 1st Helper Function that takes two arguments: (1) lis and (2) acc
-      (if (null? lis) ; Checks if the list is empty
-          acc ; Utilizes an accumulator
-          (reverse-helper (cdr lis) (cons (car lis) acc)))) ; Tail recursion
-    (reverse-helper lis '())) ; Initial accumulator
-  (reverse lis)) ; Calls the reverse function with the input list, which returns the reversed list.
+(define (my-reverse1 lis) ; Defines a new function my-reverse1 that takes a single argument 'lis'
+  (reverse-helper lis '())) ; First argument is 'lis', second argument is an empty list
 
+(define (reverse-helper lis reversed) ; 'lis' is the input list to be reversed, 'reversed' is an accumulating list that will hold the reversed elements
+  (if (null? lis) ; Checks if 'lis' is empty
+      reversed ; If 'lis' is empty, it will return 'reversed' which is the reversed list as result
+      (reverse-helper (cdr lis) (cons (car lis) reversed)))) ; Otherwise, 'cdr' removes the first element from 'lis', takes it with 'car lis', and eventually adding it to the 'resersed' list through '(cons (car lis) reversed)'
+                                                                  ; Calls 'reverse-helper' recursively without the first element of 'is' and a new list that has the first elements of 'lis' added to the front of 'reversed'
+                                                                  ; Works like a Stack data structure (FILO)
 (display "PART B: Reverse List 1")
 (newline)
-(display "The Reverse list of (11 99 66 22 88)")
-(display " is ")
-; Displays the output of my-reverse1 function when called with the input list.
-(display (my-reverse1 '(11 99 66 22 88)))
+(display "The Reverse list of (11 99 66 22 88) is ")
+(my-reverse1 '(11 99 66 22 88)) ; Calls my-reverse1 with specific 'lis' as an argument
 
 ; C. Reverse List 2
-(define (my-reverse2 lis)
-  (define (reverse-iter lis acc)
+(define (my-reverse2 lis) ; Defines a new function called 'my-reverse2' that takes a single argument 'lis'
+  (define (reverse-iter lis reversed) ; Defines an inner helper function 'reverse-iter' that takes 'lis' as the input list and 'reverse' as the list that will hold the reversed elements
     (if (null? lis) ; Checks if lis is an empty list using 'null?'. 
-        acc ; Returned when lis is empty
-        (reverse-iter (cdr lis) (cons (car lis) acc)))) ; Otherwise, the function calls itself recursively
-  (reverse-iter lis '()))
+        reversed ; If it is, then the reversed list will be returned (and also because it has also processed all the elements in 'lis')
+        (reverse-iter (cdr lis) (cons (car lis) reversed)))) ; Otherwise, 'cdr' removes the first element from 'lis', takes it with 'car lis', and eventually adding it to the 'resersed' list through '(cons (car lis) reversed)'
+                                                             ; Calls 'reverse-helper' recursively without the first element of 'is' and a new list that has the first elements of 'lis' added to the front of 'reversed'
+                                                             ; Works like a Stack data structure (FILO)
+  (reverse-iter lis '())) ; Has the effect of starting the recursion and bulds the reversed list one element at a time
 
-(newline)
 (newline)
 (display "PART C: Reversing List 2")
 (newline)
 (display "The Reverse list of (12 55 22 1 100)")
 (display " is ")
-(display (my-reverse2 '(12 55 22 1 100)))
-
+(my-reverse2 '(12 55 22 1 100)) ; Calls my-reverse2 with specific 'lis' as an argument
