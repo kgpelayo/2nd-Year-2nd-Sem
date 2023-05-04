6d863395cca03634ee2d8df87eff4f6f41a93cb5
kgpelayo <105369973+kgpelayo@users.noreply.github.com>
2023-05-04 18:18:31 +0800

    Update Carballo_Pelayo_PE06.pl

--- Carballo_Pelayo_PE06.pl
+++ Carballo_Pelayo_PE06.pl
@@ -1,34 +1,36 @@
+% Programming Exercise 6: Programming in Logic 2
+% by Meekah Yzabelle A. Carballo and Khublei Mo Satori Pelayo
 
-%   nobita has a pendant
+% Clues:
+
+% The slayer had pink eyebrows
+slayer(X) :- eyebrows(X, pink).
+
+% Nobita had a pendant
 costume(nobita, pendant).
-%   pope has G-Shock
-costume(pope, 'G-Shock').
-%   if kudo wore shades ----
-costume(kudo, fancy_bracelet).
-%   then conan wore fancy bracelet and vice versa
-costume(conan, shades).
+% Pope had a G-Shock
+costume(pope, g-shock).
+% if Kudo had a fancy bracelet, then Conan wore shades 
+costume(conan, shades) :- costume(kudo, fancy_bracelet).
+% and vice versa
+costume(kudo, shades) :- costume(conan, fancy_bracelet).
+% a person that has a fancy bracelet, then they were in room 23
+costume(X, fancy_bracelet) :- location(X, 23).
 
-%   pink eyebrows if shades
-eyebrows(X, pink) :- costume(X, shades).
-%   yellow eyebrows if fancy bracelet
-eyebrows(X, yellow) :- costume(X, fancy_bracelet).
-%   brown eyebrows if room 27
+% a person has brown eyebrows if they were in room 27
 eyebrows(X, brown) :- location(X, 27).
-%   silver eyebrows if room 25
+% a person has silver eyebrows if they were in room 25
 eyebrows(X, silver) :- location(X, 25).
+% a person has pink eyebrows if they wore the shades
+eyebrows(X, pink) :- costume(X, shades).
+% a person has yellow eyebrows if they wore a fancy bracelet
+eyebrows(X, yellow) :- costume(X, fancy_bracelet).
 
-
-%   nobita in room 25
+% nobita was in room 25
 location(nobita, 25).
-%   kudo in room 29
+% kudo was in room 29
 location(kudo, 29).
-%   conan in room 23
+% conan was in room 23
 location(conan, 23).
-%   if gshock, then room 27
-location(X, 27) :- costume(X, 'G-Shock').
-%   if fancy bracelet, room 23
-location(X, 23) :- costume(X, fancy_bracelet).
-
-
-%   slayer had (pink eyebrows)
-slayer(X) :- eyebrows(X, pink), location(X, _), costume(X, _).
+% if the person was wearing a g-shock, then he was in room 27
+location(X, 27) :- costume(X, g-shock).
